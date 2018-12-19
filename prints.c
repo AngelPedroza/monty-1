6aa58df7e3a5019626bbc486e35d29141982fd25
Athena Deng <539@holbertonschool.com>
2018-12-19 21:58:09 +0000

    first commit

---
+++ prints.c
@@ -0,0 +1,32 @@
+#include "header.h"
+
+/**
+ * op_pall - prints each stack element on a new line
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_pall(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * op_pint - prints the value at the top of the stack
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_pint(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * op_nop - does nothing
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
