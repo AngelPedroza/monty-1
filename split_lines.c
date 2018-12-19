3cd6e195f7a72ba97351d0bfb5520b45c6f3a812
Athena Deng <539@holbertonschool.com>
2018-12-19 21:00:48 +0000

    first commit

---
+++ split_lines.c
@@ -0,0 +1,24 @@
+#include "header.h"
+
+/**
+ * split_string - splits a string
+ * @line: string to be split
+ * Return: pointer to array of arrays
+ */
+char **split_string(char *line, char *delim)
+{
+	char **tokens, *token;
+	int i;
+
+	tokens = malloc(sizeof(char) * 1024);
+	if (tokens == NULL)
+		return (NULL);
+	token = strtok(line, delim);
+	for (i = 0; token != NULL; i++)
+	{
+		tokens[i] = token;
+		token = strtok(NULL, delim);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
