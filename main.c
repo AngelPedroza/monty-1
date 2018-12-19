dd09247c74f041047edf55bd6d6f3792675a38e3
Athena Deng <539@holbertonschool.com>
2018-12-19 04:07:55 +0000

    used fprintf instead of write

--- main.c
+++ main.c
@@ -8,9 +8,19 @@
  */
 int main(int ac, char **av)
 {
+	int o;
+	char *buff;
+
 	if (ac != 2)
 	{
 		write(STDERR_FILENO, "USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
+	o = open(av[1], O_RDONLY);
+	if (o == -1)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
+	
 }
