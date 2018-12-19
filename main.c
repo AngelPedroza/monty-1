b8023054edc8e86cbe889c2911ff242bce6884b1
Athena Deng <539@holbertonschool.com>
2018-12-19 21:27:55 +0000

    added more code

--- main.c
+++ main.c
@@ -8,19 +8,33 @@
  */
 int main(int ac, char **av)
 {
-	int o;
-	char *buff;
+	unsigned int lineno = 1, ind;
+	char *buffer = NULL, *newline = "\n", *sp = " "; **c_lines, **optoks;
+	size_t buffer_size;
+	FILE *stream;
 
 	if (ac != 2)
 	{
 		write(STDERR_FILENO, "USAGE: monty file\n", 18);
 		exit(EXIT_FAILURE);
 	}
-	o = open(av[1], O_RDONLY);
-	if (o == -1)
+	stream = fopen(av[1], "r");
+	if (stream == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
 		exit(EXIT_FAILURE);
 	}
-	
+	getline(&buffer, &buffer_size, stream);
+	c_lines = split_string(stream, newline);
+	for (ind = 0; c_lines[ind] != NULL; ind++)
+	{
+		optoks = split_string(c_lines[ind], sp);
+		/* here match the opcode */
+		lineno++;
+	}
+	/* loop to free everything in optoks bc malloc in split_string */
+	/* loop to free everything in c_lines bc malloc in split_string */
+	free(buffer)
+	fclose(stream);
+	return (0);
 }
