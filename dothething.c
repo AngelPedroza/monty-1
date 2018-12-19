47f3abc76580eeaac68de957ed42077f97d712e5
Athena Deng <539@holbertonschool.com>
2018-12-19 22:04:33 +0000

    first commit

---
+++ dothething.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * 
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_push(stack_t **stack, unsigned int line_number)
+{
+
+
+}
+
+/**
+ * 
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_pop(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * 
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_swap(stack_t **stack, unsigned int line_number)
+{
+
+}
+
+/**
+ * 
+ * @stack: a doubly linked list
+ * @line_number: line where opcode appears
+ */
+void op_add(stack_t **stack, unsigned int line_number)
+{
+
+}
