447ef941c8bd540eb1a544c3a2d9a3832f1e48f5
Athena Deng <539@holbertonschool.com>
2018-12-20 20:55:17 +0000

    first commit

---
+++ isint.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+/**
+ * isint - checks if a string can be converted to an integer
+ * @s: string to be checked
+ * Return: 0 if string can be converted to integer, 1 if not
+ */
+int isint(char *s)
+{
+	int i = 0;
+
+	if (s[0] == '-')
+		i++;
+	for (; s[i] != '\0'; i++)
+	{
+		if ((s[i] < '0') || (s[i] > '9'))
+			return (1);
+	}
+	return (0);
+}
