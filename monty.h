db651c790fed6902a5b01e3f89150814035a69b1
Athena Deng <539@holbertonschool.com>
2018-12-26 02:36:06 +0000

    added function prototype

--- monty.h
+++ monty.h
@@ -62,6 +62,7 @@ void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
 void op_add(stack_t **stack, unsigned int line_number);
 void op_nop(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
 
 int isint(char *s);
 char **split_string(char *line, char *delim);
